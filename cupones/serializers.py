29cbcbed8956b82124ed3bd37c4f99dec077f3a1
Emiliano Daniel Guerrero <guerrero.emiliano@outlook.com>
2025-11-26 18:57:44 -0300

    Fix import urlsafe_base64_decode

--- cupones/serializers.py
+++ cupones/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_decode
 from .models import Cuota, EstadoCuota, CuponPago, EstadoCupon, PasarelaPago, Perfil 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
